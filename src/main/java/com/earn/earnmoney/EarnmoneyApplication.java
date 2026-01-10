67024383bfa8a89cf1a8f2e5b9737717a381d91f
Karrar <alrasam9595@gmail.com>
2026-01-10 15:44:13 +0300

    revert: remove default timezone to fix counter logic

--- src/main/java/com/earn/earnmoney/EarnmoneyApplication.java
+++ src/main/java/com/earn/earnmoney/EarnmoneyApplication.java
@@ -1,17 +1,8 @@
-package com.earn.earnmoney;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
 
 @SpringBootApplication
 public class EarnmoneyApplication {
-
-	@PostConstruct
-	public void init() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Baghdad"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EarnmoneyApplication.class, args);
